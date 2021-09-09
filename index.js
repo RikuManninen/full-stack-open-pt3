e534a20a14e23236f1922c595fbe48856be92847
rikuma0 <riku.manninen@icloud.com>
2021-09-09 22:22:58 +0300

    exercise 3.6

--- index.js
+++ index.js
@@ -48,6 +48,19 @@ app.post('/api/persons', (req, res) => {
 
   const body = req.body
 
+  if(!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number is missing'
+    })
+  }
+  
+  if(persons.find(p => p.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+
   const person = {
     id: generateId(),
     name: body.name,
