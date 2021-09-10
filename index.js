0cff740cb405b0dbbda5c1afd9984f83488885a8
rikuma0 <riku.manninen@icloud.com>
2021-09-10 17:02:10 +0300

    exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req, res) {
@@ -10,6 +11,8 @@ morgan.token('body', function (req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
+
+app.use(cors())
 
 let persons = [
   { 
