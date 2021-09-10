0262c61d14f85fcc7de4fc8e45733baa7f7cdef4
rikuma0 <riku.manninen@icloud.com>
2021-09-10 15:06:27 +0300

    exercise 3.8

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
+morgan.token('body', function (req, res) {
+  if(req.method === 'POST')
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
 
